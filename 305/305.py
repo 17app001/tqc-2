fcac688b2208cf69c0c9aae2806babdace6a46d0
jerry <17app001@gmail.com>
2023-11-22 19:56:45 +0800

    完成CH401~403

--- 305/305.py
+++ 305/305.py
@@ -9,10 +9,9 @@ df_county = df1.groupby("居住縣市")["居住縣市"].count()
 print(df_county.sort_values(ascending=False))
 # 顯示感染病例人數最多的5個國家，並按遞減順序顯示
 df_country = df1.groupby("感染國家")["感染國家"].count()
-print(df_country.sort_values(ascending=False).tail(5))
+print(df_country.sort_values(ascending=False).head(5))
 # 台北市各區病例人數
 df_taipei = df1[df1.居住縣市 == "台北市"]
 print(df_taipei.groupby("居住鄉鎮")["居住鄉鎮"].count())
 # 台北市最近病例的日期
-print(df_taipei.發病日)
-print("發病日: " + df_taipei.發病日.tail(1).values[0])
+print("發病日: " + df_taipei.發病日.max())
